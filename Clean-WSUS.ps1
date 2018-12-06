1c658b7c595e5e8da10be417b786c6e64e1c8e32
Kindle, Bill <bkindle@blah.com>
2018-12-06 08:07:26 -0500

    uploaded WSUS scripts I use and added some humor to README.

---
+++ Clean-WSUS.ps1
@@ -0,0 +1,18 @@
+﻿# Performs a cleanup of WSUS. 
+# Outputs the results to a text file. 
+# Adapted and tested by BigTeddy 
+# https://gallery.technet.microsoft.com/scriptcenter/WSUS-Clean-Powershell-102f8fc6
+# 3 July 2012 
+ 
+$outFilePath = 'E:\Logs\wsusClean.txt' 
+[reflection.assembly]::LoadWithPartialName("Microsoft.UpdateServices.Administration") | out-null 
+$wsus = [Microsoft.UpdateServices.Administration.AdminProxy]::GetUpdateServer(); 
+$cleanupScope = new-object Microsoft.UpdateServices.Administration.CleanupScope; 
+$cleanupScope.DeclineSupersededUpdates = $true        
+$cleanupScope.DeclineExpiredUpdates         = $true 
+$cleanupScope.CleanupObsoleteUpdates     = $true 
+$cleanupScope.CompressUpdates                  = $true 
+#$cleanupScope.CleanupObsoleteComputers = $true 
+$cleanupScope.CleanupUnneededContentFiles = $true 
+$cleanupManager = $wsus.GetCleanupManager(); 
+$cleanupManager.PerformCleanup($cleanupScope); 
