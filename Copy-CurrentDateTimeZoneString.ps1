6bad214301dd92c9b2688881d70082dcc14cb57d
Kindle, Bill <bkindle@blah.com>
2018-12-30 10:53:27 -0500

    added a function I created today called Copy-CurrentDateTimeZoneString.ps1

---
+++ Copy-CurrentDateTimeZoneString.ps1
@@ -0,0 +1,9 @@
+# This function will copy output directly to your clipboard.
+# Created by: Bill Kindle
+# Created on: 2018-12-30 10:43:47 -0500
+# Blog: https://billkindle.github.io
+function Copy-CurrentDateTimeZoneString {
+
+    GetDate -UFormat "%Y-%m-%d %T %Z00" | clip
+    # the zero's were intentional, but they can be removed if you like.
+}
