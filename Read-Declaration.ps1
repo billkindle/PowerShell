5c9dd320718cafad528c66d4b4fafac3b83f7938
Bill Kindle <billkindle@users.noreply.github.com>
2020-07-03 13:36:39 -0400

    Update Read-Declaration.ps1

--- Read-Declaration.ps1
+++ Read-Declaration.ps1
@@ -1,16 +1,20 @@
 # Dedicated to the citizens of these United States
 
-#Requires -Version 5.1
+#This will only work (for now) with Windows PowerShell, not PowerShell 7
+if ($PSVersionTable.PSVersion -like "5.*" -eq $true) {
 
-# Setup speech synthesizer
-Add-Type -AssemblyName System.Speech
-$SpeechSynthesizer = New-Object -TypeName System.Speech.synthesis.SpeechSynthesizer
+	# Setup speech synthesizer
+	Add-Type -AssemblyName System.Speech
+	$SpeechSynthesizer = New-Object -TypeName System.Speech.synthesis.SpeechSynthesizer
 
-# Select voice, can be changed to Zira too
-$SpeechSynthesizer.SelectVoice("Microsoft David Desktop")
+	# Select voice, can be changed to Zira too
+	$SpeechSynthesizer.SelectVoice("Microsoft David Desktop")
 
-# Get Declaration of Independence text
-$Declaration = (invoke-webrequest https://gist.githubusercontent.com/billkindle/8a6283f9e109e065921cb8bdd9a5be11/raw/a6c139339e676da1ebec87dd8216c442d32eed6d/Declaration_of_Independence.txt).Content
+	# Get Declaration of Independence text
+	$Declaration = (invoke-webrequest https://gist.githubusercontent.com/billkindle/8a6283f9e109e065921cb8bdd9a5be11/raw/a6c139339e676da1ebec87dd8216c442d32eed6d/Declaration_of_Independence.txt).Content
 
-# Read the Declaration of Independance
-$SpeechSynthesizer.Speak($Declaration)
+	# Read the Declaration of Independance
+	$SpeechSynthesizer.Speak($Declaration)
+
+}
+else Write-Output "Please use Windows PowerShell to hear The Declaration of Independance read aloud."
